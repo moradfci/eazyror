49a8867b0cf9f53e82ae935e3f9b893b5e303d2d
ahmed morad <ahmedmoradfci@live.com>
2019-01-15 18:54:49 +0200

    second commit

--- app/workers/add_location_worker.rb
+++ app/workers/add_location_worker.rb
@@ -1,24 +1,29 @@
-class AddLocationWorker
-  include Sidekiq::Worker
-  # sidekiq_options retry:false
-  def perform(trip_params)
-    trip_params=JSON.parse(trip_params)
-    # historys=History.new(trip_params[:trip_id],trip_params[:latest_long],trip_params[:latest_lat]);
-    historys=History.new(trip_params);
+	class AddLocationWorker
+	  include Sidekiq::Worker
+	  # sidekiq_options retry:false
+	  def perform(trip_params)
+			trip_params=JSON.parse(trip_params)
+		
+		# historys=History.new(trip_params[:trip_id],trip_params[:latest_long],trip_params[:latest_lat]);
+			
+			historys=History.new(trip_params.trip_id,trip_params.latest_long,latest_lat);
 
-    # destenation=Trip.find(historys.trip_id);
-    # render json: {status:'Success',message:'Trip created', data:destenation}, status: :ok
-
-
-    if historys.save
-  render json: {status:'Success',message:'history created', data:historys}, status: :ok
-   else
-     render json: {status:'error saving trip',message:'Error', data:historys.errors}, status: :upprocessable_entity
-   end
+		# destenation=Trip.find(historys.trip_id);
+		# render json: {status:'Success',message:'Trip created', data:destenation}, status: :ok
 
 
+			if historys.save
+	  
+				render json: {status:'Success',message:'history created', data:historys}, status: :ok
+			else
+		 
+				render json: {status:'error saving trip',message:'Error', data:historys.errors}, status: :upprocessable_entity
+	   
+			end
 
-  end
 
 
-end
+	  end
+
+
+	end
